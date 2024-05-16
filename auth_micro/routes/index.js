e38ef158d34d123b32887d948600f5cb6bf2e4c1
Atharv Dalal <sangamdalal24@gmail.com>
2024-05-16 15:25:56 +0530

    Public route for get user

--- auth_micro/routes/index.js
+++ auth_micro/routes/index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import AuthRoutes from './authRoutes.js'
+import UserRoutes from './userRoutes.js'
 
 
 const router = express.Router()
 
 router.use('/api',AuthRoutes )
+router.use('/api', UserRoutes)
 
 export default router
