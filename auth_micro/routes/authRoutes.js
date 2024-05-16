3436b42e2f3881b53779a58c6eb1150e60131dbb
Atharv Dalal <sangamdalal24@gmail.com>
2024-05-16 15:23:36 +0530

    Private user get Route

--- auth_micro/routes/authRoutes.js
+++ auth_micro/routes/authRoutes.js
@@ -1,9 +1,14 @@
 import express from "express";
 import AuthController from "../controllers/AuthController.js";
+import authMiddlware from "../middleware/AuthMiddleware.js";
 
 const router = express.Router()
 
 
 router.post('/auth/register', AuthController.register)
+router.post('/auth/login', AuthController.login)
+
+//
+router.get("/auth/user", authMiddlware, AuthController.user)
 
 export default router
